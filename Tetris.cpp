8fec41efc062f2f67d4be784b313d02fc6b70739
Georgiy Chipunov <gchipunov@hotmail.com>
2021-02-03 22:47:26 -0800

    init1

---
+++ Tetris.cpp
@@ -0,0 +1,42 @@
+#include "Tetris.h"
+
+
+class GameBrickRaw
+{
+int iType;
+int iGameID;
+int iPersonalID;
+int iLocaltionX;
+//
+// want to pass y and z later for memory type difference
+};
+
+
+class GameMiniEngineTetrix
+{
+
+int iGameNameStringID;
+int iModMiniGame;
+void update();
+void init();
+void startOwnShaderVideoDriver(void *ptrvideo);
+
+
+
+
+};
+
+class TetrixLoop
+{
+// alloc
+// for loop
+// map collisions
+// process down
+};
+
+class InputMiniControllerXYAB
+{
+
+
+};
+class InputMiniController
